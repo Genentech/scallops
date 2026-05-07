b639ef1ab5526b7a991d9206291a89df13d9981a
Joshua Gould <joshua-gould@users.noreply.github.com>
2026-05-07 12:43:25 -0400

    Updated transform for whitening Added test to compare dask with numpy

--- scallops/tests/test_decomposition.py
+++ scallops/tests/test_decomposition.py
@@ -3,6 +3,7 @@ import dask.array as da
 import numpy as np
 import pandas as pd
 import pytest
+from sklearn.decomposition import IncrementalPCA
 
 from scallops.features.decomposition import pca
 
@@ -27,3 +28,26 @@ def test_decomposition():
     )
     np.testing.assert_array_equal(result.obs.columns, ("plate", "well"))
     assert result.X.shape == (10, 2)
+
+
+@pytest.mark.features
+def test_decomposition_compare_numpy():
+    X = da.random.random((10, 10), chunks=(2, 2))
+    adata = anndata.AnnData(X=X)
+    result = pca(
+        data=adata,
+        n_components=2,
+        standardize=False,
+        progress=False,
+        batch_size=2,
+    )
+
+    d = IncrementalPCA(n_components=2, batch_size=2)
+    result2 = d.fit_transform(X.compute())
+    np.testing.assert_array_equal(result.uns["pca"]["mean"], d.mean_)
+    np.testing.assert_array_equal(result.uns["pca"]["variance"], d.explained_variance_)
+    np.testing.assert_array_equal(
+        result.uns["pca"]["variance_ratio"], d.explained_variance_ratio_
+    )
+    np.testing.assert_array_equal(result.uns["pca"]["PCs"], d.components_)
+    np.testing.assert_almost_equal(result.X, result2)
