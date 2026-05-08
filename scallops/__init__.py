74795af4ca6c09ab0b3760b386e7ed1cdbbe1e03
Joshua Gould <joshua-gould@users.noreply.github.com>
2026-05-08 13:13:33 -0400

    hide annoying messages

--- scallops/__init__.py
+++ scallops/__init__.py
@@ -1,1 +1,18 @@
+import logging
+import warnings
+
 from .experiment.elements import Experiment  # noqa: F401
+
+logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+warnings.filterwarnings(
+    "ignore",
+    message="Unclosed client session",
+    category=ResourceWarning,
+    module="aiohttp",
+)
+warnings.filterwarnings(
+    "ignore",
+    message="Writing zarr v2 data will no longer be the default",
+    category=UserWarning,
+    module="anndata",
+)
