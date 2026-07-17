cc0f9eb15688cd1218f6d480a9082248d6dfe03f
Joshua Gould <joshua-gould@users.noreply.github.com>
2026-07-17 10:59:48 -0400

    check for null layer key

--- scallops/features/util.py
+++ scallops/features/util.py
@@ -99,24 +99,27 @@ def _slice_anndata(
     if var is not None:
         var_indices = _normalize_index(var, data.var.index)
     X = data.X
-    layers = dict(data.layers)
-    obsm = dict(data.obsm)
-    varm = dict(data.varm)
 
+    layers = dict()
+    obsm = dict()
+    varm = dict()
     if obs_indices is not None:
         X = X[obs_indices]
-        for key in layers:
-            layers[key] = layers[key][obs_indices]
-        for key in obsm:
-            obsm[key] = obsm[key][obs_indices]
+        for key in data.layers.keys():
+            if key is not None:
+                layers[key] = data.layers[key][obs_indices]
+        for key in data.obsm.keys():
+            obsm[key] = data.obsm[key][obs_indices]
     if var_indices is not None:
         X = X[:, var_indices]
-        for key in layers:
-            layers[key] = layers[key][:, var_indices]
-        for key in varm:
-            varm[key] = varm[key][var_indices]
+        for key in data.layers.keys():
+            if key is not None:
+                layers[key] = data.layers[key][:, var_indices]
+        for key in data.varm.keys():
+            varm[key] = data.varm[key][var_indices]
     obs = data.obs.iloc[obs_indices] if obs_indices is not None else data.obs
     var = data.var.iloc[var_indices] if var_indices is not None else data.var
+
     return anndata.AnnData(X=X, obs=obs, var=var, layers=layers, obsm=obsm, varm=varm)
 
 
