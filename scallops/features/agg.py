039ec95fde2984822d48339cf15aa2807d0badec
Joshua Gould <joshua-gould@users.noreply.github.com>
2026-07-22 09:00:26 -0400

    set index for agg

--- scallops/features/agg.py
+++ scallops/features/agg.py
@@ -17,10 +17,10 @@ def _weighted_median(x, weights):
 
 
 def agg_features(
-    data: anndata.AnnData,
-    by: str | Sequence[str],
-    weights_col: str | None = None,
-    agg_func: Literal["mean", "median"] = "mean",
+        data: anndata.AnnData,
+        by: str | Sequence[str],
+        weights_col: str | None = None,
+        agg_func: Literal["mean", "median"] = "mean",
 ):
     """Aggregate features
 
@@ -87,9 +87,9 @@ def agg_features(
     else:
         kwargs = dict()
         if (
-            isinstance(xdata.data, da.Array)
-            and agg_func == "mean"
-            and len(grouped.groups) > 100_000
+                isinstance(xdata.data, da.Array)
+                and agg_func == "mean"
+                and len(grouped.groups) > 100_000
         ):
             kwargs["method"] = "map-reduce"
         result = grouped.mean(**kwargs) if agg_func == "mean" else grouped.median()
@@ -114,16 +114,17 @@ def agg_features(
         index=groups,
     )
     obs = result.coords["obs"].to_dataframe()
-    obs = obs.join(group_counts, rsuffix="_1").reset_index(drop=True)
+    obs = obs.join(group_counts, rsuffix="_1")
+    # both index and column set
     if group_by_multi:
-        new_obs = pd.DataFrame(obs["obs"].tolist(), columns=by)
-        for c in obs.columns:
-            if c.startswith("count") and c not in new_obs.columns:
-                new_obs[c] = obs[c]
-        obs = new_obs
+        obs[by] = obs['obs'].apply(pd.Series)
+        obs.index = obs['obs'].map(lambda x: '-'.join(map(str, x)))
+        obs = obs.drop('obs', axis=1)
+        obs.index.name = '-'.join(by)
     else:
+        obs.index.name = by
         obs = obs.rename({"obs": by}, axis=1)
-    obs = obs.set_index(pd.RangeIndex(len(obs)).astype(str))
+
     return anndata.AnnData(
         X=X,
         obs=obs,
