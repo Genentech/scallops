fca4972ea51b056cc7d75d75ebf9b17b7c091432
Joshua Gould <joshua-gould@users.noreply.github.com>
2026-08-03 14:59:25 -0400

    hide aiohttp warnings

--- scallops/__init__.py
+++ scallops/__init__.py
@@ -3,15 +3,5 @@ import warnings
 from .experiment.elements import Experiment  # noqa: F401
 
 warnings.filterwarnings(
-    "ignore",
-    message="Writing zarr v2 data will no longer be the default",
-    category=UserWarning,
-    module="anndata",
-)
-
-warnings.filterwarnings(
-    "ignore",
-    message="anndata will no longer support zarr v2 in the near future",
-    category=DeprecationWarning,
-    module="anndata",
+    "ignore", message="unclosed.*", category=ResourceWarning, module="aiohttp"
 )
