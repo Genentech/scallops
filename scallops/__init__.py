2529ea91127b5b823587fd64aa4c950496ae4aae
Joshua Gould <joshua-gould@users.noreply.github.com>
2026-08-06 16:52:40 -0400

    warning

--- scallops/__init__.py
+++ scallops/__init__.py
@@ -5,5 +5,5 @@ from scallops.zarr_io import write_basic_dask_dask_dense  # noqa: F401
 from .experiment.elements import Experiment  # noqa: F401
 
 warnings.filterwarnings(
-    "ignore", message="unclosed.*", category=ResourceWarning, module="aiohttp"
+    "ignore", message=".*client.*", category=ResourceWarning, module="aiohttp"
 )
