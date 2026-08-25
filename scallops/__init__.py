0b6aaad8f611ad72ee71a2c9795f47e5f27bd6ee
Joshua Gould <joshua-gould@users.noreply.github.com>
2026-08-25 07:57:27 -0400

    enable rectilinear chunks

--- scallops/__init__.py
+++ scallops/__init__.py
@@ -1,6 +1,7 @@
 import warnings
 
 import anndata
+import zarr
 
 from scallops.zarr_io import write_basic_dask_dask_dense  # noqa: F401
 
@@ -11,3 +12,4 @@ warnings.filterwarnings(
     message="Unclosed client.* | client_session.*",
 )
 anndata.settings.auto_shard_zarr_v3 = False
+zarr.config.set({"array.rectilinear_chunks": True})
